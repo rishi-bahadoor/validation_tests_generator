a4bbfc72a2d93319dc94ef3fe729a79dc98f725d
rishi-bahadoor <122465036+rishi-bahadoor@users.noreply.github.com>
2025-08-07 11:10:31 -0400

    add alignments

--- excel_format.py
+++ excel_format.py
@@ -2,6 +2,13 @@ import sys
 from openpyxl import load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 from openpyxl.utils import get_column_letter
+from openpyxl.styles import Alignment
+
+def wrap_all_cells(ws):
+    wrap_alignment = Alignment(wrap_text=True)
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.alignment = wrap_alignment
 
 def find_column_letter(ws, header_name, max_search_rows):
     for row in range(1, max_search_rows + 1):
@@ -11,6 +18,13 @@ def find_column_letter(ws, header_name, max_search_rows):
                 return get_column_letter(col)
     raise ValueError(f"Header '{header_name}' not found in first {max_search_rows} rows")
 
+def find_row(ws, header_name, max_search_rows):
+    for row in range(1, max_search_rows + 1):
+        for col in range(1, ws.max_column + 1):
+            cell_value = ws.cell(row=row, column=col).value
+            if cell_value and cell_value.strip().lower() == header_name.strip().lower():
+                return row
+    raise ValueError(f"Header '{header_name}' not found in first {max_search_rows} rows")
 
 def add_drop_down(ws, column_letter,options, row_start, row_end):
     dv = DataValidation(type="list", formula1=f'"{options}"', allow_blank=True)
@@ -19,12 +33,29 @@ def add_drop_down(ws, column_letter,options, row_start, row_end):
     for row in range(row_start, row_end + 1):
         dv.add(ws[f"{column_letter}{row}"])
 
+def set_column_width(ws, header_name, width, max_search_rows=10):
+    column_letter = find_column_letter(ws, header_name, max_search_rows)
+    ws.column_dimensions[column_letter].width = width
+
+def set_drop_down(ws, header_name, options, max_search_rows=10):
+    column_letter = find_column_letter(ws, header_name, max_search_rows)
+    row_start = find_row(ws, header_name, max_search_rows) + 1
+    row_end = row_start + 94
+    add_drop_down(ws, column_letter, options, row_start, row_end)
+
 def format_excel_sheet(path):
     wb = load_workbook(path)
     ws = wb.active
 
-    column_letter = find_column_letter(ws, "Status",10)
-    add_drop_down(ws, column_letter, "Pass,Fail,Pending,Blocked", 6, 100)
+    set_drop_down(ws, "Status", "Not Started,In Progress,Completed,Blocked")
+    set_column_width(ws, "Test ID", 22)
+    set_column_width(ws, "Test Group", 18)
+    set_column_width(ws, "Priority", 10)
+    set_column_width(ws, "Description", 100)
+    set_column_width(ws, "Pass Condition", 30)
+    set_column_width(ws, "Notes", 50)
+
+    wrap_all_cells(ws)
 
     wb.save(path)
     print(f"✅ Excel formatting applied to: {path}")
