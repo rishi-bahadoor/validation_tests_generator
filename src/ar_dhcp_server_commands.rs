34e6befe800066981826c1527fe5692e83c367bf
iansooknanancepton <ian.sooknanan@cepton.com>
2025-09-18 11:41:33 -0400

    Fix comments, remove old crate

--- src/ar_dhcp_server_commands.rs
+++ src/ar_dhcp_server_commands.rs
@@ -6,7 +6,7 @@ pub fn minimal_dhcp_server_runner(trimmed_line: &str) -> Result<(), Box<dyn Erro
     // Split and collect all whitespace-separated tokens
     let args: Vec<&str> = trimmed_line.split_whitespace().collect();
 
-    // 1. Validate we have the token token: [start or stop]
+    // Validate we have the token token: [start or stop]
     if args.len() < 1 {
         return Err(
             "Incorrect amount of arguments: Usage: minimal_dchp_server <offered_ip>".into(),
@@ -15,8 +15,10 @@ pub fn minimal_dhcp_server_runner(trimmed_line: &str) -> Result<(), Box<dyn Erro
 
     // Resolve the .py location
     let script = script_path("minimal_dhcp_server.py")?;
-    // Give sensor time to carry out the DHCP operation.
-    // Also used by the server to terminate in the event we don't
+
+    // Specifies how long to run the server for.
+    // 20 seconds was chosen to give sensor time to carry out the DHCP operation.
+    // It is also used by the server to self terminate in the event we don't.
     let runtime = 20.to_string();
     let offered_ip = args[1];
     let status = Command::new("python")
